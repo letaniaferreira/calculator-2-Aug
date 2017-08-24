30a54678260d1d9162b1e03fa2e377107f36d374
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-24 14:02:21 -0700

    added operations. py and automated input

--- calculator.py
+++ calculator.py
@@ -10,62 +10,146 @@ from arithmetic import *
 # Your code goes here
 
 
+def automated(file_name):
+
+    math_operation = open(file_name)
+
+    for line in math_operation:
+        operator, num1, num2 = line.split()
+
+    # tokens = input_string.split(' ')
+
+        operator = tokens[0]
+
+        operators = ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']
+
+        if operator not in operators:
+            print 'Invalid input'
+            continue
+
+        if len(tokens) >= 2:
+
+            num1 = int(tokens[1])
+
+        if len(tokens) == 3:
+
+            num2 = int(tokens[2])
+
+        nums = tokens[1:]
+        nums_list = []
+
+        for num in nums:
+            nums_list.append(int(num))
+
+        if operator == '+':
+            print '{:.2f}'.format(my_reduce(add, nums_list))
+
+        elif operator == '-':
+            print '{:.2f}'.format(my_reduce(subtract, nums_list))
+
+        elif operator == '*':
+            print '{:.2f}'.format(my_reduce(multiply, nums_list))
+
+        elif operator == '/':
+            print '{:.2f}'.format(my_reduce(divide, nums_list))
+
+        elif operator == 'square':
+            print '{:.2f}'.format(square(num1))
+
+        elif operator == 'cube':
+            print '{:.2f}'.format(cube(num1))
+
+        elif operator == 'pow':
+            print '{:.2f}'.format(my_reduce(power, nums_list))
+
+        elif operator == 'mod':
+            print '{:.2f}'.format(my_reduce(mod, nums_list))
+
+        else:
+            print 'Your input is invalid'
+
+
+
+automated('operations.txt')
+
 # loop until user enters 'q'
+def my_reduce(function, lst):
 
+    accum = lst[0]
+
+    for item in lst[1:]:
+        accum = function(accum, item)
+
+    return accum
+
+
+def user_input_calculator():
 # while input_string != 'q' or input_string != 'quit'
-while True:
-    input_string = raw_input('> ')
+    while True:
+        input_string = raw_input('> ')
 
-    if input_string == 'q' or input_string == 'quit':
-        break
+        if input_string == 'q' or input_string == 'quit':
+            break
 
-    tokens = input_string.split(' ')
+        tokens = input_string.split(' ')
 
-    operator = tokens[0]
+        operator = tokens[0]
 
-    operators = ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']
+        operators = ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']
 
-    if operator not in operators:
-        print 'Invalid input'
-        continue
+        if operator not in operators:
+            print 'Invalid input'
+            continue
 
-    if len(tokens) >= 2:
+        if len(tokens) >= 2:
 
-        num1 = int(tokens[1])
+            num1 = int(tokens[1])
 
-    if len(tokens) == 3:
+        if len(tokens) == 3:
 
-        num2 = int(tokens[2])
+            num2 = int(tokens[2])
 
-    if operator == '+':
-        print add(num1, num2)
+        nums = tokens[1:]
+        nums_list = []
 
-    elif operator == '-':
-        print subtract(num1, num2)
+        for num in nums:
+            nums_list.append(int(num))
 
-    elif operator == '*':
-        print multiply(num1, num2)
+        if operator == '+':
+            print '{:.2f}'.format(my_reduce(add, nums_list))
 
-    elif operator == '/':
-        print divide(num1, num2)
+        elif operator == '-':
+            print '{:.2f}'.format(my_reduce(subtract, nums_list))
 
-    elif operator == 'square':
-        print square(num1)
+        elif operator == '*':
+            print '{:.2f}'.format(my_reduce(multiply, nums_list))
 
-    elif operator == 'cube':
-        print cube(num1)
+        elif operator == '/':
+            print '{:.2f}'.format(my_reduce(divide, nums_list))
 
-    elif operator == 'pow':
-        print power(num1, num2)
+        elif operator == 'square':
+            print '{:.2f}'.format(square(num1))
 
-    elif operator == 'mod':
-        print mod(num1, num2)
+        elif operator == 'cube':
+            print '{:.2f}'.format(cube(num1))
 
-    else:
-        print 'Your input is invalid'
+        elif operator == 'pow':
+            print '{:.2f}'.format(my_reduce(power, nums_list))
+
+        elif operator == 'mod':
+            print '{:.2f}'.format(my_reduce(mod, nums_list))
+
+        else:
+            print 'Your input is invalid'
 
 
 
+
+
+
+# iterate through list, calling the function on the accumulator
+    # assign updated value to the accumlator with each iteration
+# return final accumulated value
 
 
 
